6229a73edd905532e4630414ce1a9e4d501b89ae
Iliana <106244484+ilianaRapti@users.noreply.github.com>
2025-08-18 17:03:28 +0300

    new quiz page

---
+++ src/contents/quiz.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Quiz: React.FC = () => {
+
+    return(
+        <div>
+            <h1>QUIZ</h1>
+            <p>This is a test</p>
+        </div>
+    );
+};
+
+export default Quiz;
