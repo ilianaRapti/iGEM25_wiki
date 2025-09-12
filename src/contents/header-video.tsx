31215d9e7aa4093016a575f008310d8e25621b33
Iliana <106244484+ilianaRapti@users.noreply.github.com>
2025-09-12 19:03:03 +0300

    changed video header to div

--- src/contents/header-video.tsx
+++ src/contents/header-video.tsx
@@ -3,24 +3,21 @@ import "./header-video.css";
 
 const VideoHeader: React.FC = () => {
   return (
-    <header className="video-header">
+    <div className="video-header"> {/* άλλαξα από <header> σε <div> */}
       <iframe
         className="video-bg"
         title="IOANNINA: Shots of plane trees (2025)" 
-        width="560" 
-        height="315" 
-        src="https://video.igem.org/videos/embed/gHSpjovmWGmTNojoTWrL8M?autoplay=1&amp;title=0&amp;warningTitle=0&amp;controlBar=0&amp;peertubeLink=0&muted=1&loop=1"
+        src="https://video.igem.org/videos/embed/gHSpjovmWGmTNojoTWrL8M?autoplay=1&title=0&warningTitle=0&controlBar=0&peertubeLink=0&muted=1&loop=1"
         sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-presentation"
-        allow="autoplay; fullscreen; picutre-in-picutre;"
+        allow="autoplay; fullscreen; picture-in-picture"
         allowFullScreen
       ></iframe>
 
       <div className="overlay"></div>
       <div className="header-content">
-        {/*<h1>iGEM IOANNINA 2025</h1>
-        <p>AMADRYADS</p>*/}
+        {/* Εδώ μπορείς να βάλεις κείμενο ή κουμπιά πάνω από το video */}
       </div>
-    </header>
+    </div>
   );
 };
 
